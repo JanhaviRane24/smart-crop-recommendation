c36eb7e40aa0980901e32b9cbd148108c7506427
Janhavi jayram rane <158069012+JanhaviRane24@users.noreply.github.com>
2026-07-12 23:05:02 +0530

    Update API URL for authentication requests

--- smart-crop-recommendation/frontend/src/api/auth.js
+++ smart-crop-recommendation/frontend/src/api/auth.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL; // http://127.0.0.1:8000/accounts
+const BASE_URL = import.meta.env.VITE_API_ACCOUNTS; // https://smart-crop-backend-hq3t.onrender.com/api/accounts
 
 // Login
 export const loginUser = async ({ username, password }) => {
-  const res = await axios.post(`${API}/login/`, { username, password });
+  const res = await axios.post(`${BASE_URL}/login/`, { username, password });
   return res.data;
 };
 
 // Register
 export const registerUser = async ({ username, password, email }) => {
-  const res = await axios.post(`${API}/register/`, { username, password, email }, { withCredentials: true });
+  const res = await axios.post(`${BASE_URL}/register/`, { username, password, email }, { withCredentials: true });
   return res.data;
 };
