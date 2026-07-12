06fe7e6b11aabb3fbba8a1b37f692082777bb06a
Janhavi jayram rane <158069012+JanhaviRane24@users.noreply.github.com>
2026-07-12 23:30:52 +0530

    Update CORS settings for allowed origins and headers

--- smart-crop-recommendation/backend/backend/settings.py
+++ smart-crop-recommendation/backend/backend/settings.py
@@ -67,11 +67,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://smart-crop-frontend-4fgk.onrender.com",
 ]
-CORS_ALLOW_HEADERS = list(default_headers) + [
+CORS_ALLOW_CREDENTIALS = TrueCORS_ALLOW_HEADERS = list(default_headers) + [
     "api-key",
 ]
 
