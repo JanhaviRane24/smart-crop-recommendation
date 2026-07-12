50ba8add975525124b9d0f26795bc1c7891066a3
Janhavi jayram rane <158069012+JanhaviRane24@users.noreply.github.com>
2026-07-12 22:27:09 +0530

    Change API variable to BASE_URL for authentication

--- smart-crop-recommendation/frontend/src/api/auth.js
+++ smart-crop-recommendation/frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_ACCOUNTS; // http://127.0.0.1:8000/accounts
+const BASE_URL = import.meta.env.VITE_API_URL; // http://127.0.0.1:8000/accounts
 
 // Login
 export const loginUser = async ({ username, password }) => {
