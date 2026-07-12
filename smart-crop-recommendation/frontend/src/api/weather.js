d66fd5a952d907d246785e87c77e53aea39cb5a9
Janhavi jayram rane <158069012+JanhaviRane24@users.noreply.github.com>
2026-07-12 23:51:49 +0530

    Update weather.js

--- smart-crop-recommendation/frontend/src/api/weather.js
+++ smart-crop-recommendation/frontend/src/api/weather.js
@@ -1,5 +1,5 @@
 export const getWeather = async (city, apiKey) => {
-  const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
   if (!res.ok) {
     throw new Error("City not found or API error");
   }
